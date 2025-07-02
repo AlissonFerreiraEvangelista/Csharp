0e6fc8a3942f379718d4a602cb1ea499e7ce7bc0
Alisson Ferreira <alisson.22559@gmail.com>
2025-07-02 20:27:00 -0300

    Primeiro commit

---
+++ Atividades/Atividades/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Atividades { 
+
+    class Program {
+        static void Main(string[] args) {
+            // Exemplo de uso do método Somar
+            int resultado = Somar(5, 10);
+            Console.WriteLine($"A soma de 5 e 10 é: {resultado}");
+        }
+        // Método que recebe dois inteiros e retorna a soma deles
+        static int Somar(int a, int b) {
+            return a + b;
+        }
+    }
+
+}
