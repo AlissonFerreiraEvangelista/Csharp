f30d03df9c7e8117e77d5a514790f885f44287fc
Alisson Ferreira <alisson.22559@gmail.com>
2025-07-07 20:01:25 -0300

    Comentar operações matemáticas e adicionar novas classes
    O código foi atualizado para comentar chamadas de métodos nas classes `Dia1` e `Dia2`, desativando sua execução. Foram adicionadas as classes `Dia3`, `Dia4` e `Dia5`, que introduzem funcionalidades como tabuada, contagem regressiva e cálculo de média de notas. A estrutura geral do programa foi mantida.

--- Atividades/Atividades/Program.cs
+++ Atividades/Atividades/Program.cs
@@ -2,15 +2,18 @@
 
     class Program {
         static void Main(string[] args) {
-         
-            Dia1.ehImpar(28);
-            Dia2.Soma(10.5, 20.3);
-            Dia2.Subtracao(10.5, 20.3);
-            Dia2.Multiplicacao(10.5, 20.3);
-            Dia2.Divisao(10.5, 20.3);
-            Dia2.Modulo(10.5, 20.3);
-            Dia2.Exponenciacao(2, 20);
-            Dia2.RaizQuadrada(144);
+
+            //Dia1.ehImpar(28);
+            //Dia2.Soma(10.5, 20.3);
+            //Dia2.Subtracao(10.5, 20.3);
+            //Dia2.Multiplicacao(10.5, 20.3);
+            //Dia2.Divisao(10.5, 20.3);
+            //Dia2.Modulo(10.5, 20.3);
+            //Dia2.Exponenciacao(2, 20);
+            //Dia2.RaizQuadrada(144);
+            //Dia3.Tabuala();
+            //Dia4.ContagemRegressiva(10);
+            Dia5.MediaNotas(7.5, 8.0, 9.0);
         }
          
     }
@@ -26,8 +29,6 @@
 
         }
     }
-
-
     public static class Dia2 {
         public static void Soma(double x, double y) {
             double resultado = x + y;
@@ -77,6 +78,51 @@
             Console.WriteLine($"A raiz quadrada de {x} é igual a {resultado}");
         }
     }
+    public static class Dia3 {
+        public static void Tabuala() {
+
+            Console.WriteLine("Digite um número para ver sua tabuada:");
+            int numero = int.Parse(Console.ReadLine() ?? "0");
+
+            for (int i=0; i<10;i++) {
+                int resultado = numero * (i + 1);
+                Console.WriteLine($"{numero} x {i + 1} = {resultado}");
+
+            }
+        }
+    }
+    public static class Dia4 {
+
+        public static void ContagemRegressiva(int numero) {
+
+            if (numero <= 0) {
+                Console.WriteLine("Número negativo ou Zero não é permitido.");
+                return;
+            }
+
+            Console.WriteLine("Contagem Regressiva:");
+            for (int i = 0; i < numero; i++) { 
+                Console.WriteLine(numero - i);
+            }
+        }
+        
+
+    }
+    public static class Dia5 { 
+
+        public static void MediaNotas(double nota1, double nota2, double nota3) {
+            
+            double[] notas = { nota1, nota2, nota3 };
+
+            double soma = 0;
+            for (int i = 0; i < notas.Length; i++) {       
+                soma += notas[i];
+            }
+            Console.WriteLine($"A média das notas é: {soma / notas.Length}");
+        }
+
+    }
+
 
 
 }
